22acc8fe9a313508a5598702aeb94ce4cd61d49f
KhalkarYash <yashmk2004@gmail.com>
2025-02-12 23:22:32 +0530

    Add API call to fetch now playing movies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const logo =
 
 export const user_avatar =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYThmNDEwODQ0OWU4Yzk4ZjVkNTM5ZmFhM2IyYWVlNiIsIm5iZiI6MTczOTM4MDk3My44NSwic3ViIjoiNjdhY2Q4ZWRiOWIxNzkyNGUyZDBhODAzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.K4VYPHwjmVJPEH7QUHeH7yHbHMtrwPBISdt-4mcRPtU",
+  },
+};
