ac1a378864e0b28d1fa176a384520735aee351fe
KhalkarYash <yashmk2004@gmail.com>
2025-02-13 02:29:27 +0530

    Refactor video components and add global CSS styles

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -6,16 +6,17 @@ const VideoBackground = ({ movieId, title }) => {
   useMovieTrailer(movieId);
 
   return (
-    <iframe
-      width="560"
-      height="315"
-      src={"https://www.youtube.com/embed/" + trailerVideo?.key}
-      title={title}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-    ></iframe>
+    <div className="w-screen">
+      <iframe
+        className="w-screen aspect-video"
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&modestbranding=1&rel=0&controls=1&showinfo=0&loop=1&playlist=${trailerVideo?.key}`}
+        title={title}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
   );
 };
 
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,15 +1,15 @@
 const VideoTitle = (props) => {
   const { title, overview } = props;
   return (
-    <div className="pt-36 px-12">
+    <div className="absolute w-screen aspect-video pt-80 px-12 text-white bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/2">{overview}</p>
-      <div className="w-1/5 flex justify-between">
-        <button className="text-black rounded-md bg-white p-4 px-12 text-lg bg-opacity-50 hover:bg-opacity-80">
-          Play
+      <div className="">
+        <button className="text-black font-semibold rounded-md bg-white p-4 px-12 text-lg bg-opacity-90 hover:bg-opacity-50">
+          <i class="fa-solid fa-play"></i> Play
         </button>
-        <button className="mx-2 text-white rounded-md bg-gray-300 p-4 px-7 text-lg bg-opacity-80 hover:bg-opacity-90">
-          More info
+        <button className="mx-2 text-white rounded-md bg-gray-500 p-4 px-7 text-lg bg-opacity-30 hover:bg-opacity-50">
+          <i class="fa-solid fa-circle-info"></i> More info
         </button>
       </div>
     </div>
