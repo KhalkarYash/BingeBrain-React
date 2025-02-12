86d0fdc14032d1f247412d41a888b1fa88260106
KhalkarYash <yashmk2004@gmail.com>
2025-02-13 00:36:16 +0530

    VideoTitle & MainContainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -7,10 +7,11 @@ const MainContainer = () => {
   if (!movies) return;
   const mainMovie = movies[0];
   console.log(mainMovie);
+  const { original_title, overview } = mainMovie;
 
   return (
     <div>
-      <VideoTitle />
+      <VideoTitle title={original_title} overview={overview} />
       <VideoBackground />
     </div>
   );
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,7 +1,19 @@
-const VideoTitle = () => {
+const VideoTitle = (props) => {
+  const { title, overview } = props;
   return (
-    <div>VideoTitle</div>
-  )
-}
+    <div className="pt-36 px-12">
+      <h1 className="text-6xl font-bold">{title}</h1>
+      <p className="py-6 text-lg w-1/2">{overview}</p>
+      <div className="w-1/5 flex justify-between">
+        <button className="text-black rounded-md bg-white p-4 px-12 text-lg bg-opacity-50 hover:bg-opacity-80">
+          Play
+        </button>
+        <button className="mx-2 text-white rounded-md bg-gray-300 p-4 px-7 text-lg bg-opacity-80 hover:bg-opacity-90">
+          More info
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default VideoTitle
+export default VideoTitle;
